b3093968093b6bc82c2ff7bb49112a1b7e16e7d4
Jaime Suarez <jaisua8@gmail.com>
2026-02-17 22:26:46 -0500

    Creacion de las validaciones de medico en los campos de la tabla

---
+++ Modelo/Metadatos/Medico.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Modelos
+{
+    [MetadataType(typeof(MedicoMetaData))]
+    public partial class Medico
+    {
+    }
+
+    public class MedicoMetaData
+    {
+        [Required]
+        [StringLength(10)]
+        public string cedula { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo no puede tener más de 50 caracteres")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo no puede tener más de 50 caracteres")]
+        public string apellidoPaterno { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo no puede tener más de 50 caracteres")]
+        public string apellidoMaterno { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public string esEspecialista { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        public string habilitado { get; set; }
+    }
+}
