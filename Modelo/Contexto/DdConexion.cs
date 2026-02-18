37dcae5ba8c51dd9dfa1d2dc58e979dd1a29e230
Jaime Suarez <jaisua8@gmail.com>
2026-02-17 22:13:09 -0500

    config de la DdConexion

---
+++ Modelo/Contexto/DdConexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Modelos
+{
+    public partial class DdConexion: DbContext
+    {
+        private DbConexion( string stringConexion)
+            : base(stringConexion)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Database.CommandTimeout = 900;
+        }
+
+        public static DdConexion Create()
+        {
+            return new DdConexion("name=db_clinicaEntities");
+        }
+    }
+}
