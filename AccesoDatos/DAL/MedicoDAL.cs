75872a1b44b9168c93d96391f16b0926f4c18ae2
Jaime Suarez <jaisua8@gmail.com>
2026-02-18 09:16:40 -0500

    Creacion  de los view models y el listado paginado

--- AccesoDatos/DAL/MedicoDAL.cs
+++ AccesoDatos/DAL/MedicoDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comun.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,10 @@ namespace AccesoDatos.DAL
 {
     public class MedicoDAL
     {
-        LeerTodos()
+        public static ListadoPaginadoVMR<MedicoVMR> LeerTodos(int cantidad, int pagina, string textoBusqueda)
         {
-
+            ListadoPaginadoVMR<MedicoVMR> resultado = new ListadoPaginadoVMR<MedicoVMR>();
+            return resultado;
         }
         LeerUno()
         {
