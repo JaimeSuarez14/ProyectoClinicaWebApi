4a7deb38a9ace47e36760a86dd4ce53c923985c7
Jaime Suarez <jaisua8@gmail.com>
2026-02-17 22:48:10 -0500

    Creacion de las validaciones de egreso en los campos de la tabla

--- Modelo/Metadatos/Egreso.cs
+++ Modelo/Metadatos/Egreso.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Modelo.Modelos
 {
+    [MetadataType(typeof( EgresoMetadatos))]
     public partial class Egreso
     {
+    }
+
+    public class EgresoMetadatos
+    {
+        [Required]
+        public System.DateTime fecha { get; set; }
+        [Required]
+        [Range(0,99999999999.99)]
+        public decimal monto { get; set; }
+        [Required]
+        public long medicoId { get; set; }
+        [Required]
+        public long ingresoId { get; set; }
     }
 }
