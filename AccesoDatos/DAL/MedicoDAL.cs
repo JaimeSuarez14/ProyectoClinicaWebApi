224ec403b7d44a253eafa858893bd730d3eca40c
Jaime Suarez <jaisua8@gmail.com>
2026-02-18 09:00:10 -0500

    Creacion de las Clases de Acceso a Datos

---
+++ AccesoDatos/DAL/MedicoDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.DAL
+{
+    public class MedicoDAL
+    {
+        LeerTodos()
+        {
+
+        }
+        LeerUno()
+        {
+
+        }
+        Crear()
+        {
+
+        }
+        Actualizar()
+        {
+
+        }
+        Eliminar()
+        {
+
+        }
+    }
+}
