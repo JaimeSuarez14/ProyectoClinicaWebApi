b671c24e361fad959337b92e06af1829152a4eab
Jaime Suarez <jaisua8@gmail.com>
2026-02-17 22:54:01 -0500

    Añade metadatos y validaciones para Ingreso
    Se agrega el archivo Ingreso.cs en Metadatos, definiendo la clase parcial Ingreso y su clase de metadatos con validaciones mediante DataAnnotations para los campos requeridos. Se actualiza Modelo.csproj para incluir este nuevo archivo en la compilación.

---
+++ Modelo/Metadatos/Ingreso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Metadatos
+{
+    [MetadataType(typeof(IngresoMetaData))]
+    public partial class Ingreso
+    {
+    }
+
+    public partial class IngresoMetaData
+    {
+        [Required(ErrorMessage = "La fecha es requerida.")]
+        public System.DateTime fecha { get; set; }
+        [Required(ErrorMessage = "El número de sala es requerido.")]
+        public int numeroSala { get; set; }
+        [Required(ErrorMessage = "El número de cama es requerido.")]
+        public int numeroCama { get; set; }
+        [Required(ErrorMessage = "El diagnóstico es requerido.")]
+        public string diagnostico { get; set; }
+        [Required(ErrorMessage = "El médico es requerido.")]
+        public long medicoId { get; set; }
+        [Required(ErrorMessage = "El paciente es requerido.")]
+        public long pacienteId { get; set; }
+    }
+}
